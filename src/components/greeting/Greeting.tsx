e8747e11cb39bdf3fa1a1d463dafe67f679cb3de
MikaelHolmqvist2020 <holmqvistmikael2020@gmail.com>
2021-01-29 17:52:07 +0100

    mappstruktur

---
+++ src/components/greeting/Greeting.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Greeting.css'
+
+export const Greeting = () => {
+  return (
+    <div>
+      <h1 className="title">Hello Awesom website ok</h1>
+    </div>
+  )
+}
