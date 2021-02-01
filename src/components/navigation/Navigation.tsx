97af50309b6705838ad9fb72058389b6ee3aaeba
MikaelHolmqvist2020 <holmqvistmikael2020@gmail.com>
2021-02-01 17:07:06 +0100

    lektion 01-02-2021

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Navigation.css'
+import { MobileNavigation } from './mobilenavigation/MobileNavigation'
+import { DesktopNavigation } from './desktopnavigation/DesktopNavigation'
+import { useWindowDimensions } from '../../hooks/useWindowDimensions'
 
 export const Navigation = () => {
+  const { height, width } = useWindowDimensions()
+
+  const displayNavigationDependingOnDevice = () => {
+    return (width <= 1000)
+      ? <MobileNavigation />
+      : <DesktopNavigation />
+  }
+
   return (
     <div>
-      
+      {displayNavigationDependingOnDevice()}
     </div>
   )
 }
---
+++ src/components/navigation/mobilenavigation/sidebar/SideBar.tsx
@@ -0,0 +1,14 @@
+import './SideBar.css'
+import React from 'react'
+
+export const SideBar = (props: {drawerIsOpen: boolean, drawerHandler: Function }) => {
+  return (
+    <div className={props.drawerIsOpen ? 'side-drawer open' : 'side-drawer'}>
+      <button onClick={() => props.drawerHandler(false)}>Stäng Menyn</button>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        </ul>
+    </div>
+  )
+}
